3b4997a9d54540b157ad59ba0c28934fc8b1ff29
Alexander Kurz <alexhkurz@gmail.com>
2024-05-17 18:48:41 -0700

    Added new file disco.py

---
+++ disco.py
@@ -0,0 +1,12 @@
+from discopy import Ty, Word, Cup, Cap, Diagram, Id, Functor
+from discopy.rigid import Functor
+
+s, n = Ty('s'), Ty('n')  # s for sentence, n for noun
+
+alice = Word("Alice", n)
+bob = Word("Bob", n)
+loves = Word("loves", n.r @ s @ n.l)
+
+sentence = alice @ loves @ bob >> Id(n) @ Cup(n, n.r) @ Id(n.l)
+print("Diagram of the sentence 'Alice loves Bob':")
+sentence.draw(figsize=(6, 4), fontsize=12)
